c3c0b50bbc0e33521e176351626cb9a4bc46bd43
JX001 <3455074@qq.com>
2019-07-24 15:30:05 +0800

    选择排序

--- study/src/main/java/com/jjx/study/SelectionSort.java
+++ study/src/main/java/com/jjx/study/SelectionSort.java
@@ -2,29 +2,31 @@ package com.jjx.study;
 
 /**
  * 选择排序
+ * 
  * @author jiangjinxin
  */
 public class SelectionSort {
 	public static void main(String[] args) {
-		int[] arr= {9,3,2,4,7,8};
+		int[] arr = { 9, 3, 2, 4, 7, 8 };
 		selectionSort(arr);
-		for(int i: arr) {
-    		System.out.println(i);
-    	}
+		for (int i : arr) {
+			System.out.println(i);
+		}
 	}
+
 	public static void selectionSort(int[] arr) {
 		int min;
 		for (int i = 0; i < arr.length; i++) {
-			min=i;
-			 for (int j = i+1; j < arr.length; j++) {
-				if (arr[min]>arr[j]) {
-					min=j;
-				} 
+			min = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[min] > arr[j]) {
+					min = j;
+				}
 			}
-			int temp=arr[i];
-			arr[i]=arr[min];
-			arr[min]=temp;
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
 		}
-		
+
 	}
 }
